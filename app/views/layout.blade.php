02b14f764678304396af156a282bdb43c70f4b65
John Coggeshall <john@coggeshall.org>
2016-06-22 14:53:52 -0400

    An experiment for the UI of the magic mirror

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -27,73 +27,75 @@
 	
 </head>
 <body>
-	<div class="region fullscreen below">
-	   <div class="container">
-	   {{ $fullscreen_below }}
-	   </div>
-	</div>
-	<div class="region top bar">
-		<div class="container">
-		  {{ $top_bar }}
-		</div>
-		<div class="region top left">
-		  <div class="container">
-		  {{ $top_left }}
-		  </div>
-		</div>
-		<div class="region top center">
-		  <div class="container">
-		  {{ $top_center }}
-		  </div>
-		</div>
-		<div class="region top right">
-		  <div class="container">
-		  {{ $top_right }}
-		  </div>
-	    </div>
-	</div>
-	<div class="region upper third">
-	   <div class="container">
-	   {{ $upper_third }}
-	   </div>
-	</div>
-	<div class="region middle center">
-	   <div class="container">
-	   {{ $middle_center }}
-	   </div>
-	</div>
-	<div class="region lower third">
-	   <div class="container">
-	   {{ $lower_third }}
-	   </div>
-	</div>
-	<div class="region bottom bar">
-		<div class="container">
-		{{ $bottom_bar }}
-		</div>
-		<div class="region bottom left">
-		  <div class="container">
-		  {{ $bottom_left }}
-		  </div>
-		</div>
-		<div class="region bottom center">
-		  <div class="container">
-		  {{ $bottom_center }}
-		  @if(isset($exception))
-		  {{ $exception }}
-		  @endif
-		  </div>
-		</div>
-		<div class="region bottom right">
-		  <div class="container">
-		  {{ $bottom_right }}
-		  </div>
-	    </div>
-	</div>
-	<div class="region fullscreen above">
-	   <div class="container">
-	   {{ $fullscreen_above }}
-	   </div>
-	</div>
+    <canvas id="c" style="position: absolute; top:0; left:0; width:100%;height:100%; "></canvas>
+    	<div class="region fullscreen below">
+    	   <div class="container">
+    	   {{ $fullscreen_below }}
+    	   </div>
+    	</div>
+    	<div class="region top bar">
+    		<div class="container">
+    		  {{ $top_bar }}
+    		</div>
+    		<div class="region top left">
+    		  <div class="container">
+    		  {{ $top_left }}
+    		  </div>
+    		</div>
+    		<div class="region top center">
+    		  <div class="container">
+    		  {{ $top_center }}
+    		  </div>
+    		</div>
+    		<div class="region top right">
+    		  <div class="container">
+    		  {{ $top_right }}
+    		  </div>
+    	    </div>
+    	</div>
+    	<div class="region upper third">
+    	   <div class="container">
+    	   {{ $upper_third }}
+    	   </div>
+    	</div>
+    	<div class="region middle center">
+    	   <div class="container">
+    	   {{ $middle_center }}
+    	   </div>
+    	</div>
+    	<div class="region lower third">
+    	   <div class="container">
+    	   {{ $lower_third }}
+    	   </div>
+    	</div>
+    	<div class="region bottom bar">
+    		<div class="container">
+    		{{ $bottom_bar }}
+    		</div>
+    		<div class="region bottom left">
+    		  <div class="container">
+    		  {{ $bottom_left }}
+    		  </div>
+    		</div>
+    		<div class="region bottom center">
+    		  <div class="container">
+    		  {{ $bottom_center }}
+    		  @if(isset($exception))
+    		  {{ $exception }}
+    		  @endif
+    		  </div>
+    		</div>
+    		<div class="region bottom right">
+    		  <div class="container">
+    		  {{ $bottom_right }}
+    		  </div>
+    	    </div>
+    	</div>
+    	<div class="region fullscreen above">
+    	   <div class="container">
+    	   {{ $fullscreen_above }}
+    	   </div>
+    	</div>
 </body>
 </html>
+<script src="/js/water.js"></script>
